d71717bc57b8a7d92c4f7690755a5fab859e1adb
luii2606 <valemidx@gmail.com>
2025-03-28 21:11:37 -0500

    base de datos distinta

--- servidor/server.js
+++ servidor/server.js
@@ -4,6 +4,18 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+server.use(
+  jsonServer.rewriter({
+    "/artistas": "/artists",
+    "/movimientos": "/movements",
+    "/obras": "/artworks",
+    "/museos": "/museums",
+    "/artistas/:id": "/artists/:id",
+    "/movimientos/:id": "/movements/:id",
+    "/obras/:id": "/artworks/:id",
+    "/museos/:id": "/museums/:id"
+  })
+);
 server.use(router)
 server.listen(3000, () => {
   console.log('JSON Server is running')
